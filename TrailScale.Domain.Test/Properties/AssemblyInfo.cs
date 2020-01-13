2c6d361e6255528be0c70fbfce54fe48db06b697
Jase Hackman <Jase.Hackman@healthstream.com>
2020-01-13 14:48:34 -0600

    Created a basic test to test Trip

---
+++ TrailScale.Domain.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TrailScale.Domain.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TrailScale.Domain.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("108e9a67-1e28-4f3f-b389-fd4ca288ef70")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TrailScale.Domain.Test/UT_Trip.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrailScale.Domain;
+
+namespace TrailScale.Domain.Test
+{
+    [TestClass]
+    public class UT_Trip
+     {
+        #region Properties
+        Weather mWeather;
+        Trip mTrip;
+        Trail mTrail;
+
+        #endregion
+
+        [TestMethod]
+        public void ShouldCreateANewTrip()
+        {
+            GivenATrailObj();
+            GivenAWeatherObj();
+            WhenATripIsCreated();
+        }
+
+        public void GivenAWeatherObj()
+        {
+            mWeather = new Weather(Precipitation.Snow, Temperature.AboveSeventyAndBelowNinety);
+        }
+        public void GivenATrailObj()
+        {
+            mTrail = new Trail(10.4, TrailDifficulty.Easy, "AwesomeTrail");
+        }
+
+        public void WhenATripIsCreated()
+        {
+            mTrip = new Trip("TheBestTripEver", mWeather, mTrail);
+        }
+
+        public void ThenATripIsNotNull()
+        {
+            Assert.IsNotNull(mTrip);
+        }
+    }
+
+}
